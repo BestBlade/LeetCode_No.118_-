566881cfd3d53299f598d7820ea777958a3da41a
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-01-09 13:47:57 +0800

    Add files via upload

---
+++ leetcode_no118_杨辉三角_w1.cpp
@@ -0,0 +1,89 @@
+﻿/* ------------------------------------------------------------------|
+给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+
+
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+示例:
+
+输入: 5
+输出:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/pascals-triangle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：6.7 MB, 在所有 C++ 提交中击败了66.27%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> res(numRows);
+    if (!numRows) {
+        return res;
+    }
+    for (int i = 0; i < numRows; i++) {
+        res[i].resize(i + 1);
+        res[i][0] = res[i][i] = 1;
+        for (int j = 1; j < i; j++) {
+            res[i][j] = res[i - 1][j] + res[i - 1][j - 1];
+        }
+    }
+    return res;
+}
+
+int main() {
+    int numRows = 5;
+    generate(numRows);
+}
